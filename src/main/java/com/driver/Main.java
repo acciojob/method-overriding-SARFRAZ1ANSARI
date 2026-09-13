f032bcc97c45fa639966523b029b95fee4fe1484
SARFRAZ1ANSARI <117897621+SARFRAZ1ANSARI@users.noreply.github.com>
2026-09-13 17:48:51 +0530

    changes cone

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,24 +1,34 @@
 package com.driver;
+// Task 1: Create class A
 class A {
 
     public String meth() {
         return "Invoking method from class A";
     }
 }
+
+
+// Task 2: Create class B which extends class A
 class B extends A {
 
+    // Task 4: Override meth() from class A
     @Override
     public String meth() {
         return "Method is overridden in Extended class B";
     }
+}
+
+
+// Main class
+public class Main {
 
     public static void main(String[] args) {
 
-        // Task 3
+        // Task 3: Create object of class B
         B obj = new B();
-        System.out.println(obj.meth());
 
-        // Task 5
+        // Task 5: Call overridden method
         System.out.println(obj.meth());
     }
 }
+
